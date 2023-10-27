d7e7717485db05c24604213784163f3e997a97d9
gEoNNNN <eugen.ostafi@isa.utm.md>
2023-10-27 12:22:35 +0300

    creating the functions number_of_methodes,number_of_clases,number_of_lines for AllFiles class

--- Lab3/programFiles.py
+++ Lab3/programFiles.py
@@ -7,3 +7,25 @@ class ProgramFiles(AllFiles):
             self.lineCount = lineCount
             self.classCount = classCount
             self.methodCount = methodCount
+
+    def number_of_lines(self,file_path):
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+            line_count = len(lines)
+        return line_count
+    
+    def number_of_clases(self,file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+            class_pattern = re.compile(r'class\s+\w+\s*[{(]')
+            classes = class_pattern.findall(content)
+            class_count = len(classes)
+        return class_count
+        
+    def number_of_methodes(self,file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+            method_pattern = re.compile(r'def\s+\w+\s*\([^)]*\)\s*[{(]')
+            methods = method_pattern.findall(content)
+            method_count = len(methods)
+        return method_count     
