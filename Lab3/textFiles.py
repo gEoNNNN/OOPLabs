98b4938da4a47497de7ae7236bec56c699898bc9
gEoNNNN <eugen.ostafi@isa.utm.md>
2023-10-27 12:41:00 +0300

    creating the functions number_of_lines,number_of_words,number_of_charaters in the TextFiles, and moveing the last commit function from allFunction to ProgramFiles

--- Lab3/textFiles.py
+++ Lab3/textFiles.py
@@ -6,3 +6,22 @@ class TextFiles(AllFiles):
         self.lineCount = lineCount
         self.wordCount = wordCount
         self.characterCount = characterCount
+
+    def number_of_lines(self,file_path):
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+            line_count = len(lines)
+        return line_count
+    
+    def number_of_words(self,file_path):
+        with open(file_path, "r") as file:
+            content = file.read()
+            words = content.split()
+        word_count = len(words)
+        return word_count
+    
+    def number_of_charaters(self,file_path):
+        with open(file_path, "r") as file:
+            content = file.read()
+            character_count = len(content)
+        return character_count
