f2625a276065065f7a9e74a32df1a871d07aaca4
gEoNNNN <eugen.ostafi@isa.utm.md>
2023-10-27 10:14:38 +0300

    createing get_size function that return the size in pixels of image

--- Lab3/imageFiles.py
+++ Lab3/imageFiles.py
@@ -5,3 +5,9 @@ class ImageFiles(AllFiles):
     def __init__(self, name, extension, created, updated, imageSize):
         super().__init__(name, extension, created, updated)
         self.imageSize = imageSize
+    
+    def get_size(self,file_path):
+        image = Image.open(file_path)
+        width, height = image.size
+        size = str(width) + "x" + str(height)
+        return size
