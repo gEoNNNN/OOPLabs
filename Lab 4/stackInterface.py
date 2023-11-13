3af1c82b9c83785dcdd3b44dbff77b532d2789c9
gEoNNNN <eugen.ostafi@isa.utm.md>
2023-11-13 21:45:59 +0200

    Add Abstract Stack Interface

---
+++ Lab 4/stackInterface.py
@@ -0,0 +1,16 @@
+class StackInterface:
+    def push(self, item):
+        """Add an item to the top of the stack."""
+        raise NotImplementedError
+
+    def pop(self):
+        """Remove the top item from the stack."""
+        raise NotImplementedError
+
+    def peek(self):
+        """Look at the top item without removing it."""
+        raise NotImplementedError
+
+    def is_empty(self):
+        """Check if the stack is empty."""
+        raise NotImplementedError
