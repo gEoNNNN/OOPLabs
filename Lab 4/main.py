2997ea2ae6dfa88eef8d508bc7a45c1fb9d488ef
gEoNNNN <eugen.ostafi@isa.utm.md>
2023-12-04 13:17:15 +0200

    final changes

---
+++ Lab 4/main.py
@@ -0,0 +1,6 @@
+from testing import DataStructureTester
+
+if __name__ == "__main__":
+    tester = DataStructureTester()
+    tester.test_stacks()
+    tester.test_queues()
--- Lab 4/testing.py
+++ Lab 4/testing.py
@@ -2,57 +2,57 @@ from arrayStack import ArrayStack
 from linkedStack import LinkedStack
 from arrayQueue import ArrayQueue
 from linkedQueue import LinkedQueue
-from circluareQueue import CircularQueue  
-from dictStack import DictStack  
+from circluareQueue import CircularQueue
+from dictStack import DictStack
 
-def test_stacks():
-    print("Testing ArrayStack:")
-    array_stack = ArrayStack(5)
-    array_stack.push(1)
-    array_stack.push(2)
-    print(array_stack.pop())  # Expected output: 2
-    print(array_stack.peek())  # Expected output: 1
+class DataStructureTester:
+    @staticmethod
+    def test_stacks():
+        print("Testing ArrayStack:")
+        array_stack = ArrayStack(5)
+        array_stack.push('a')
+        array_stack.push('b')
+        print(array_stack.pop())  # Expected: b
+        print(array_stack.peek())  # Expected: a
 
-    print("\nTesting LinkedStack:")
-    linked_stack = LinkedStack()
-    linked_stack.push('a')
-    linked_stack.push('b')
-    print(linked_stack.pop())  # Expected output: 'b'
-    print(linked_stack.peek())  # Expected output: 'a'
+        print("\nTesting LinkedStack:")
+        linked_stack = LinkedStack()
+        linked_stack.push(1)
+        linked_stack.push(2)
+        print(linked_stack.pop())  # Expected: 2
+        print(linked_stack.peek())  # Expected: 1
 
-    print("\nTesting DictStack:")
-    dict_stack = DictStack()
-    dict_stack.push('key1', 'value1')
-    dict_stack.push('key2', 'value2')
-    print(dict_stack.pop())  # Expected output: ('key2', 'value2')
-    print(dict_stack.peek())  # Expected output: ('key1', 'value1')
+        print("\nTesting DictStack:")
+        dict_stack = DictStack()
+        dict_stack.push('obj2', 'val1')
+        dict_stack.push('obj1', 'val2')
+        print(dict_stack.pop())  # Expected: ('obj1', 'val2')
+        print(dict_stack.peek())  # Expected: ('obj2', 'val1')
 
-def test_queues():
-    print("\nTesting ArrayQueue:")
-    array_queue = ArrayQueue(4)
-    array_queue.enqueue('apple')
-    array_queue.enqueue('banana')
-    print(array_queue.dequeue())  # Expected output: 'apple'
-    print(array_queue.peek())  # Expected output: 'banana'
+    @staticmethod
+    def test_queues():
+        print("\nTesting ArrayQueue:")
+        array_queue = ArrayQueue(4)
+        array_queue.enqueue('car')
+        array_queue.enqueue('bike')
+        print(array_queue.dequeue())  # Expected: 'car'
+        print(array_queue.peek())  # Expected: 'bike'
 
-    print("\nTesting LinkedQueue:")
-    linked_queue = LinkedQueue()
-    linked_queue.enqueue(10)
-    linked_queue.enqueue(20)
-    print(linked_queue.dequeue())  # Expected output: 10
-    print(linked_queue.peek())  # Expected output: 20
+        print("\nTesting LinkedQueue:")
+        linked_queue = LinkedQueue()
+        linked_queue.enqueue(10)
+        linked_queue.enqueue(20)
+        print(linked_queue.dequeue())  # Expected: 10
+        print(linked_queue.peek())  # Expected: 20
 
-    print("\nTesting CircularQueue:")
-    circular_queue = CircularQueue(3)
-    circular_queue.enqueue('red')
-    circular_queue.enqueue('green')
-    circular_queue.enqueue('blue')
-    print(circular_queue.dequeue())  # Expected output: 'red'
-    print(circular_queue.peek())  # Expected output: 'green'
-    circular_queue.enqueue('yellow')
-    print(circular_queue.dequeue())  # Expected output: 'green'
-    print(circular_queue.peek())  # Expected output: 'blue'
+        print("\nTesting CircularQueue:")
+        circular_queue = CircularQueue(3)
+        circular_queue.enqueue('orange')
+        circular_queue.enqueue('apple')
+        circular_queue.enqueue('banana')
+        print(circular_queue.dequeue())  # Expected: 'orange'
+        print(circular_queue.peek())  # Expected: 'apple'
+        circular_queue.enqueue('kiwi')
+        print(circular_queue.dequeue())  # Expected: 'apple'
+        print(circular_queue.peek())  # Expected: 'banana'
 
-if __name__ == "__main__":
-    test_stacks()
-    test_queues()
